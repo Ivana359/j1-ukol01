357bd75d4ff5003051d3f420d4732ab144afc6f3
rempa359 <rempa@seznam.cz>
2023-02-15 07:37:51 +0100

    Nakreslí se prasátko

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -7,11 +7,80 @@ public class HlavniProgram {
     public void start() {
         Turtle zofka = new Turtle();
 
-        //TODO implementace domácího úkolu
+
+        //Nakresli prasatko
+        prasatko(zofka);
+
+
+
+    }
+
+    private static void prasatko(Turtle zofka) {
+        zofka.turnLeft(90);
+        nakresliDomecek(zofka);
+        zofka.turnLeft(90);
+        zofka.move(100);
+
+        nozicky(zofka);
+
+        zofka.move(150);
+        zofka.turnLeft(90);
+
+        nozicky(zofka);
+
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.turnLeft(45);
+        zofka.move(50);
+        zofka.turnLeft(180);
+        zofka.move(50);
+        zofka.turnLeft(45);
+        zofka.move(100);
+        zofka.turnRight(180);
+    }
+
+    private static void nozicky(Turtle zofka) {
+        zofka.turnRight(45);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnRight(90);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnLeft(45);
+    }
+
+    private static void nakresliDomecek(Turtle zofka) {
+        nakresliObdelnik(zofka);
+        zofka.move(150);
+        zofka.turnLeft(30);
+        zofka.move(100);
+        zofka.turnLeft(120);
+        zofka.move(100);
+        zofka.turnLeft(30);
+        zofka.move(150);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnLeft(90);
+    }
+
+
+    private static void nakresliObdelnik(Turtle zofka) {
+        for (int i = 0; i < 2; i++) {
+                zofka.move(150);
+                zofka.turnLeft(90);
+                zofka.move(100);
+                zofka.turnLeft(90);
+
+            }
     }
 
     public static void main(String[] args) {
         new HlavniProgram().start();
+
     }
 
 }
