c15acfd89045b1d64c81468609717031838f68f5
rempa359 <rempa@seznam.cz>
2023-02-15 09:16:13 +0100

    Neprve se nakreslí prasátko, poté se želva přesune na novou pozici, nakreslí osmiuhelnik, přesune se na další pozici - nakreslí kolečko, přesune se na další pozici a nakreslí sluníčko.

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -11,7 +11,36 @@ public class HlavniProgram {
         //Nakresli prasatko
         prasatko(zofka);
 
+        //zofka se presune na novou pozici a nakresli osmiuhelnik
+        zofka.setLocation(200, 200);
+        nUhelnik(zofka, 50, 8);
 
+
+        //zofka se presune na novou pozici a nakresli kolecko
+        zofka.setLocation(400, 200);
+        nUhelnik(zofka, 10, 36);
+
+        //zofka se presune na novou pozici a nakreslí slunicko
+        zofka.setLocation(600, 200);
+        nakresliSlunicko(zofka, 10);
+
+
+    }
+
+
+    private static void nUhelnik(Turtle zofka, double strana, double pocetStran) {
+        double uhel = 360 / pocetStran;
+        for (int i = 0; i < pocetStran; i++) {
+            zofka.move(strana);
+            zofka.turnRight(uhel);
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        new HlavniProgram().start();
 
     }
 
@@ -67,20 +96,33 @@ public class HlavniProgram {
         zofka.turnLeft(90);
     }
 
-
     private static void nakresliObdelnik(Turtle zofka) {
         for (int i = 0; i < 2; i++) {
-                zofka.move(150);
-                zofka.turnLeft(90);
-                zofka.move(100);
-                zofka.turnLeft(90);
+            zofka.move(150);
+            zofka.turnLeft(90);
+            zofka.move(100);
+            zofka.turnLeft(90);
+
+        }
+    }
+
+    private void nakresliSlunicko(Turtle zofka, double delkaStrany) {
+        double uhel = 360.0 / 27;
+        for (int i = 0; i < 9; i++) {
+
+
+            for (int j = 0; j < 3; j++) {
+                zofka.move(delkaStrany);
+                zofka.turnRight(uhel);
 
             }
+            zofka.turnLeft(90);
+            zofka.move(50);
+            zofka.turnLeft(180);
+            zofka.move(50);
+            zofka.turnLeft(90);
+        }
+
+    }
     }
 
-    public static void main(String[] args) {
-        new HlavniProgram().start();
-
-    }
-
-}
